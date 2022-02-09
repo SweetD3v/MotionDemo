0b7b260bc9466d114e2ff0979b8eaf3f618ba940
Dev4Life <vishrut.goyani9@gmail.com>
2022-02-09 11:47:30 +0530

    Initial commit

---
+++ app/src/main/java/com/dev4life/multipermission/MainActivity.java
@@ -0,0 +1,110 @@
+package com.dev4life.multipermission;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import com.dev4life.multipermission.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+    ArrayList<String> permissionsList;
+    String[] permissionsStr = {
+            Manifest.permission.CAMERA,
+            Manifest.permission.RECORD_AUDIO,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.ACCESS_FINE_LOCATION
+    };
+    int permissionsCount = 0;
+
+    ActivityResultLauncher<String[]> permissionsLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(),
+                    new ActivityResultCallback<Map<String, Boolean>>() {
+                        @RequiresApi(api = Build.VERSION_CODES.M)
+                        @Override
+                        public void onActivityResult(Map<String, Boolean> result) {
+                            ArrayList<Boolean> list = new ArrayList<>(result.values());
+                            permissionsList = new ArrayList<>();
+                            permissionsCount = 0;
+                            for (int i = 0; i < list.size(); i++) {
+                                if (shouldShowRequestPermissionRationale(permissionsStr[i])) {
+                                    permissionsList.add(permissionsStr[i]);
+                                } else if (!hasPermission(MainActivity.this, permissionsStr[i])) {
+                                    permissionsCount++;
+                                }
+                            }
+                            if (permissionsList.size() > 0) {
+                                askForPermissions(permissionsList);
+                            } else if (permissionsCount > 0) {
+                                //Show alert dialog
+                                showPermissionDialog();
+                            } else {
+                                //All permissions granted. Do your stuff 🤞
+                                binding.txtStatus.setText("All permissions are granted!");
+                            }
+                        }
+                    });
+
+    private boolean hasPermission(Context context, String permissionStr) {
+        return ContextCompat.checkSelfPermission(context, permissionStr) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    AlertDialog alertDialog;
+
+    private void showPermissionDialog() {
+        binding.txtStatus.setText("Showing settings dialog");
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Permission required")
+                .setMessage("Some permissions are needed to be allowed to use this app without any problems.")
+                .setPositiveButton("Ok", (dialog, which) -> {
+                    dialog.dismiss();
+                });
+        if (alertDialog == null) {
+            alertDialog = builder.create();
+            if (!alertDialog.isShowing()) {
+                alertDialog.show();
+            }
+        }
+    }
+
+    private void askForPermissions(ArrayList<String> permissionsList) {
+        String[] newPermissionStr = new String[permissionsList.size()];
+        for (int i = 0; i < newPermissionStr.length; i++) {
+            newPermissionStr[i] = permissionsList.get(i);
+        }
+        if (newPermissionStr.length > 0) {
+            binding.txtStatus.setText("Asking for permissions");
+            permissionsLauncher.launch(newPermissionStr);
+        } else showPermissionDialog();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        permissionsList = new ArrayList<>();
+        permissionsList.addAll(Arrays.asList(permissionsStr));
+        askForPermissions(permissionsList);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+}
