2c2bef65a3c3bdcdc2aa880f9e6e478294dd2bd2
Dev4Life <33550494+SweetD3v@users.noreply.github.com>
2023-07-05 18:31:00 +0530

    Create DynamicHeightImageView.kt

---
+++ DynamicHeightImageView.kt
@@ -0,0 +1,70 @@
+package com.demo.staggeredexample
+
+import android.content.Context
+import android.util.AttributeSet
+import com.google.android.material.imageview.ShapeableImageView
+
+
+class DynamicHeightImageView : ShapeableImageView {
+    private var mAspectRatio = 1f
+
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    )
+
+
+    fun setAspectRatio(aspectRatio: Float) {
+        mAspectRatio = aspectRatio
+        requestLayout()
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val measuredWidth = measuredWidth
+        setMeasuredDimension(measuredWidth, (measuredWidth / mAspectRatio).toInt())
+    }
+}
+
+
+
+
+
+
+//            if (adapterPosition % 5 == 0) flowerItemBinding.image.setAspectRatio(9 / 16f)
+//            if (adapterPosition % 3 == 0) flowerItemBinding.image.setAspectRatio(12 / 18f)
+//            if (adapterPosition % 7 == 0) flowerItemBinding.image.setAspectRatio(1f)
+
+
+
+
+
+<?xml version="1.0" encoding="utf-8"?>
+<androidx.constraintlayout.widget.ConstraintLayout xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:app="http://schemas.android.com/apk/res-auto"
+    android:layout_width="match_parent"
+    android:layout_height="wrap_content"
+    android:padding="2dp">
+
+    <androidx.constraintlayout.widget.ConstraintLayout
+        android:layout_width="match_parent"
+        android:layout_height="match_parent"
+        android:background="?android:attr/selectableItemBackground">
+
+        <com.demo.staggeredexample.DynamicHeightImageView
+            android:id="@+id/image"
+            android:layout_width="0dp"
+            android:layout_height="wrap_content"
+            android:adjustViewBounds="true"
+            android:scaleType="centerCrop"
+            app:layout_constraintEnd_toEndOf="parent"
+            app:layout_constraintStart_toStartOf="parent"
+            app:layout_constraintTop_toTopOf="parent"
+            app:shapeAppearanceOverlay="@style/ShapeAppearance8" />
+
+    </androidx.constraintlayout.widget.ConstraintLayout>
+
+</androidx.constraintlayout.widget.ConstraintLayout>
